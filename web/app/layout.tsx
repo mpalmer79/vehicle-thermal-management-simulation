0393be68a43d087ecbe57fbbcea11cc6109ff855
Michael Palmer <mpalm2025@gmail.com>
2026-08-16 15:38:14 -0400

    UI-4: load light theme overrides

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import "./ui2.css";
+import "./ui4.css";
 import { AppShell } from "@/components/app-shell";
 
 export const metadata: Metadata = {
